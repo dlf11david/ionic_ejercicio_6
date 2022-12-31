4b436f9951518f2ec273f0015e67b866e3172d61
davidlopez <dfl11@alu.ua.es>
2022-12-31 19:53:59 +0100

    Ejercicio 6.1 - Nuevo servicio

--- iVideoclub/src/app/services/peliculas-api.service.ts
+++ iVideoclub/src/app/services/peliculas-api.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PeliculasAPIService {
 
-  constructor() { }
+  APIep = 'http://gbrain.dlsi.ua.es/videoclub/api/v1/catalog';
+
+  constructor(public http: HttpClient) { }
+
+  getPeliculas(): any {
+
+    return this.http.get(this.APIep);
+
+  }
+
+  getPelicula(id: number): any {
+
+    return this.http.get(this.APIep + '/' + id);
+
+  }
+
 }
